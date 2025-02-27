cd463f83ac0fa6aa7ef6f03c3ee770774baf698e
Rodolfo Henrique <viniciustrajanobs@gmail.com>
2025-02-27 15:22:15 -0300

    chore: removing unecessary comments

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -3,58 +3,58 @@ import { IUserRepository } from "../repositories/UserRepository";
 import * as jwt from "jsonwebtoken";
 
 export interface IUserService {
-  createUser(data: {
-    name: string;
-    email: string;
-    password: string;
-  }): Promise<User>;
-  login(
-    email: string,
-    password: string
-  ): Promise<{ user: User; token: string }>;
+	createUser(data: {
+		name: string;
+		email: string;
+		password: string;
+	}): Promise<User>;
+	login(
+		email: string,
+		password: string
+	): Promise<{ user: User; token: string }>;
 }
 
 export class UserService implements IUserService {
-  constructor(private userRepository: IUserRepository) {}
+	constructor(private userRepository: IUserRepository) {}
 
-  async createUser(data: {
-    name: string;
-    email: string;
-    password: string;
-  }): Promise<User> {
-    const existingUser = await this.userRepository.findByEmail(data.email);
-    if (existingUser) {
-      throw new Error("Email is already in use!");
-    }
+	async createUser(data: {
+		name: string;
+		email: string;
+		password: string;
+	}): Promise<User> {
+		const existingUser = await this.userRepository.findByEmail(data.email);
+		if (existingUser) {
+			throw new Error("Email is already in use!");
+		}
 
-    const user = new User();
-    user.name = data.name;
-    user.email = data.email;
-    user.password = data.password;
+		const user = new User();
+		user.name = data.name;
+		user.email = data.email;
+		user.password = data.password;
 
-    return this.userRepository.create(user);
-  }
+		return this.userRepository.create(user);
+	}
 
-  async login(
-    email: string,
-    password: string
-  ): Promise<{ user: User; token: string }> {
-    const user = await this.userRepository.findByEmail(email);
-    if (!user) {
-      throw new Error("User not found!");
-    }
+	async login(
+		email: string,
+		password: string
+	): Promise<{ user: User; token: string }> {
+		const user = await this.userRepository.findByEmail(email);
+		if (!user) {
+			throw new Error("User not found!");
+		}
 
-    const isPasswordValid = await user.verifyPassword(password);
-    if (!isPasswordValid) {
-      throw new Error("Invalid password!");
-    }
+		const isPasswordValid = await user.verifyPassword(password);
+		if (!isPasswordValid) {
+			throw new Error("Invalid password!");
+		}
 
-    const token = jwt.sign(
-      { userId: user.id, email: user.email },
-      process.env.JWT_SECRET || "seu-segredo-aqui",
-      { expiresIn: "1h" } // Token expira em 1 hora
-    );
+		const token = jwt.sign(
+			{ userId: user.id, email: user.email },
+			process.env.JWT_SECRET || "seu-segredo-aqui",
+			{ expiresIn: "1h" }
+		);
 
-    return { user, token };
-  }
+		return { user, token };
+	}
 }
